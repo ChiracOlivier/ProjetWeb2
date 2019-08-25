516effcd85eeb27227a6b3a64066baf6f05d89bb
Chiracolivier <chiracolivier28@gmail.com>
2019-08-25 21:39:25 +0200

    method doGet() in main class.

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -1,4 +1,21 @@
 package com.demo;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Main", urlPatterns = "/Main")
+public class Main extends HttpServlet {
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String message = "Transmission de variables : OK !";
+    request.setAttribute("test", message);
+    this.getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+  }
+
 
 }
